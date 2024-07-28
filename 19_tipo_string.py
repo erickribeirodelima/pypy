9a8c9aedbf13dbab8701197d55bade70b4f596dc
Erick Ribeiro <eri.05@hotmail.com>
2024-07-28 14:06:23 -0300

    adicionando dois novos arquivos

---
+++ 19_tipo_string.py
@@ -0,0 +1,8 @@
+# Aspas simples
+print('Erick Ribeiro')
+
+# Aspas duplas
+print("Miguel Correia")
+
+# Escape é a barra invertida
+print("João de \"Lima")         # João de "Lima
---
+++ 20_tipo_int_float.py
@@ -0,0 +1,15 @@
+# Tipos int e tipos float
+# int = inteiro
+print(11)
+print(-11)
+print(0)
+
+# float = números com ponto flutuante
+# float sem sinal é considerado um número positivo
+print(10.10)
+print(9.05)
+numero_float = 20.5
+print(type(numero_float))
+
+print(type('Erick'))
+print(type(1))
