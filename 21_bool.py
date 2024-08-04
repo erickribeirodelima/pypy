405031a9c93bcee6fd23d55c350c1f9fb2e83234
Erick Ribeiro <eri.05@hotmail.com>
2024-08-04 18:29:10 -0300

    Novos arquivos de domingo dia 04/08/2024

---
+++ 21_bool.py
@@ -0,0 +1,13 @@
+# Tipo bool (boolean)
+# Questionar algo em um programa
+# Só existem dois (True) ou (False)
+# Existem alguns operadores para questionar, tipo == para questionar se um valor é igual ao outro
+
+numero1 = 12
+numero2 = 12
+numero3 = 10
+
+print(numero1 == numero2) # True
+print(numero1 == numero3) # False
+print(numero1 > numero3) # True
+print(numero3 < numero2) # True
