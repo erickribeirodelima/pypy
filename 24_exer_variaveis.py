e475e0dcd84a3d679a32c000f2ad54d39a6013bd
Erick RIBEIRO DE LIMA <73002502@ombr.int.adeo.com>
2024-08-06 14:07:01 -0300

    Primeiro exercicio

---
+++ 24_exer_variaveis.py
@@ -0,0 +1,13 @@
+nome = 'Erick'
+sobrenome = 'Ribeiro de Lima'
+idade = 33
+ano_nascimento = 2024 - idade
+maior_idade = idade >= 18
+altura = 1.84
+
+print('Nome: ', nome)
+print('Sobrenome: ', sobrenome)
+print('Idade: ', idade)
+print('Ano de nascimento: ', ano_nascimento)
+print('Maior de idade: ', maior_idade)
+print('Altura: ', altura)
