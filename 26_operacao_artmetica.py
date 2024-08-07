0c07d348ded69eb7c1f3b18b0eaba25e42ecf4b0
Erick Ribeiro <eri.05@hotmail.com>
2024-08-06 21:54:35 -0300

    Subindo um novo arquivo

---
+++ 26_operacao_artmetica.py
@@ -0,0 +1,24 @@
+adicao = 10 + 10
+print(adicao)
+
+subtracao = 20 - 10
+print(subtracao)
+
+multiplicacao = 2 * 10
+print(multiplicacao)
+
+divisao = 10 / 2.2
+print(divisao)
+
+divisao_inteira = 10 // 2.2
+print(divisao_inteira)
+
+exponenciacao = 2 ** 10
+print(exponenciacao)
+
+modulo = 55 % 2         # resto da divisão
+print(modulo)
+
+print(10 % 2 == 0) # True
+print(9 % 2 == 0) # False
+print(9 % 1 == 0) # True
