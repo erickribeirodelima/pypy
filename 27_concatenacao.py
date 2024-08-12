e2c3587a97dc8a0bdcce800ff4b9b9b16d310859
Erick <eri.05@hotmail.com>
2024-08-12 18:49:08 -0300

    Novos Arquivos - 20240812

---
+++ 27_concatenacao.py
@@ -0,0 +1,9 @@
+# concatenação
+
+concatenacao = 'Erick' + ' ' + 'Ribeiro' + ' ' + 'Lima'
+print(concatenacao)
+
+a_dez_vezes = 'A' * 10
+tres_vezes_erick = 3 * 'Erick'
+print(a_dez_vezes)
+print(tres_vezes_erick)
