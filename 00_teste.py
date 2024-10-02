719cb8166b6a899ed3441f48a4fda4954391ade6
Erick <eri.05@hotmail.com>
2024-10-01 23:32:09 -0300

    New desk

--- 00_teste.py
+++ 00_teste.py
@@ -1,8 +1,1 @@
-senha = input('Digite a senha: ')
-
-if not senha:
-    print('Você não digitou nada.')
-elif senha == '1234':
-    print('Acesso realizado com sucesso.')
-else:
-    print('Senha inválida!')
+print("Hello new desk")
